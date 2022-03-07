7d027a9cccbebd3399823848d097443ed533cd39
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-03-07 03:19:30 -0300

    navigation page finished

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
 //Navbar clique
 const navToggler = document.querySelector(".jsNavToggler");
-const nav = document.querySelector(".jsNav");
+const navList = document.querySelector(".jsNav");
+const openButtonMenu = document.querySelector(".open");
+const closeButtonMenu = document.querySelector(".close");
 
 function navToggle() {
-  nav.classList.toggle("active");
+  navList.classList.toggle("active");
+  navToggler.classList.toggle("active");
+  openButtonMenu.classList.toggle("active");
+  closeButtonMenu.classList.toggle("active");
+}
+
+//Esconder a nav bar caso seja clicado fora do menu
+function compareClickInOrOut() {
+  document.addEventListener("click", (event) => {
+    const isClickInsideNav = navList.contains(event.target);
+    const isClickInsideNavToggler = navToggler.contains(event.target);
+
+    if (
+      !(isClickInsideNav || isClickInsideNavToggler) &&
+      navList.classList.contains("active")
+    ) {
+      navToggle();
+    }
+  });
 }
 
 navToggler.addEventListener("click", navToggle);
+navToggler.addEventListener("click", compareClickInOrOut);
+
+
+
+
